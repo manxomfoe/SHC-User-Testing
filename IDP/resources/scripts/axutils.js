e1571f5b9f2198f513fa5d554702b3e53c9fa495
manxomfoe <brian.duchek@gmail.com>
2013-06-19 14:34:45 -0500

    Tweaking Resources.js
    Some weird code breaking in the background of the pages causing .js to
    be printed into the body.

--- IDP/resources/scripts/axutils.js
+++ IDP/resources/scripts/axutils.js
@@ -67,7 +67,7 @@
      };
 
      _axUtils.loadJS = function(url) {
-         $('head').append('<script text="text/javascript" language="JavaScript" src="' + url + '"></script>');
+         $('head').append('<script text="text/javascript" language="JavaScript" src="' + url + '"></sc'+'ript>');
      };
 
      _axUtils.curry = function(fn) {
